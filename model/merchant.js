d7c94bcbc8d51a384c77a41b52621924c3b95e10
Tuguldur Bayarsaikhan <Tuguuldur.0624@gmail.com>
2024-04-25 21:56:44 +0800

    Merchant хэсэг засав

--- model/merchant.js
+++ model/merchant.js
@@ -2,13 +2,11 @@ const mongoose = require("mongoose");
 
 const MerchantSchema = mongoose.Schema({
   name: { type: String, required: true },
-  reg_no: { type: String, required: true },
   activity_type: { type: String, required: true },
   employee: { type: Number, required: true },
   year: { type: Number, required: true },
   web: { type: String, required: true },
-  address: { type: mongoose.Schema.ObjectId, ref: "Address" },
-  req_type: { type: String, required: true, enum: ["Авах", "Зарах"] },
+  number: { type: Number, required: true },
 });
 
 const Merchant = mongoose.model("Merchant", MerchantSchema);
